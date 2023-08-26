3a384b68f3a5b1d7d72b494a627192f80051c1dc
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2023-08-27 02:17:56 +0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -8,6 +8,137 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Usern
 UPDATES_CHANNEL = C.UPDATES_CHANNEL
 logging.basicConfig(level=logging.INFO)
 
+@Client.on_message(filters.incoming & filters.command(['bottycu1']) & filters.private)
+async def _start(client, message):
+    update_channel = UPDATES_CHANNEL
+    if update_channel:
+        try:
+            user = await client.get_chat_member(update_channel, message.chat.id)
+            if user.status == "kicked":
+               await client.send_message(
+                   chat_id=message.chat.id,
+                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/ACE_OffTopic).",
+                   parse_mode="markdown",
+                   disable_web_page_preview=True
+               )
+               return
+        except UserNotParticipant:
+            await client.send_message(
+                chat_id=message.chat.id,
+                text="**Please Join My Updates Channel to use this Bot!**",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{update_channel}")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            await client.send_message(message.chat.id,
+                text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
+	        reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                           InlineKeyboardButton("Join Updates Channel", url="https://t.me/ACE_ML"),
+                           InlineKeyboardButton("Support Group", url="https://t.me/ACE_OffTopic")
+                      ],
+                     [
+                           InlineKeyboardButton("🧑‍💻Devloper🧑‍💻", url="https://t.me/FlashSpeedster1")
+                     ]
+                 ]
+             ),
+        parse_mode="markdown",
+        reply_to_message_id=message.message_id
+        )
+            return
+    await client.send_message(message.chat.id,
+        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
+	reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Join Updates Channel", url="https://t.me/ACE_ML"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/ACE_OffTopic")
+                ],
+                [
+                    InlineKeyboardButton("🧑‍💻Devloper🧑‍💻", url="https://t.me/FlashSpeedster1")
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+        reply_to_message_id=message.message_id
+        )
+
+
+@Client.on_message(filters.incoming & filters.command(['source_code6']) & filters.private)
+async def _source_code(client, message):
+    await client.send_message(message.chat.id,
+        text=tr.SC_MSG.format(message.from_user.first_name, message.from_user.id),
+	reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("🌐 Source Code 🌐", url="https://t.me/ACE_ML")
+                ],
+                [
+                    InlineKeyboardButton("🌟 Join Channel", url="https://t.me/ACE_ML"),
+                    InlineKeyboardButton("💬 Support Group", url="https://t.me/ACE_OffTopic")
+                ],
+                [
+                    InlineKeyboardButton("👨‍💻 Developer 🧑‍💻", url="https://t.me/FlashSpeedster1")
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+        reply_to_message_id=message.message_id
+        )
+
+@Client.on_message(filters.incoming & filters.command(['helpmemk']) & filters.private)
+async def _help(client, message):
+    update_channel = UPDATES_CHANNEL
+    if update_channel:
+        try:
+            user = await client.get_chat_member(update_channel, message.chat.id)
+            if user.status == "kicked":
+               await client.send_message(
+                   chat_id=message.chat.id,
+                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/ACE_OffTopic).",
+                   parse_mode="markdown",
+                   disable_web_page_preview=True
+               )
+               return
+        except UserNotParticipant:
+            await client.send_message(
+                chat_id=message.chat.id,
+                text="**Please Join My Updates Channel to use this Bot!**",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{update_channel}")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            await client.send_message(
+                chat_id=message.chat.id,
+                text="Hey use this command in my pm. \nFor more help ask in my [Support Group](https://t.me/ACE_OffTopic).",
+                parse_mode="markdown",
+                disable_web_page_preview=True)
+            return
+    await client.send_message(chat_id = message.chat.id,
+        text = tr.HELP_MSG[1],
+        disable_notification = True,
+        reply_markup = InlineKeyboardMarkup(map(1)),
+        reply_to_message_id = message.message_id
+    )
+
+help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
+
+
 @Client.on_callback_query(help_callback_filter)
 async def help_answer(client, callback_query):
     chat_id = callback_query.from_user.id
