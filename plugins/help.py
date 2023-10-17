b8b824a6fb2f3f3e0bd9dcb79ed09fe817db7446
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2023-10-17 16:56:57 +0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -80,7 +80,7 @@ async def _start(client, message):
         )
 
 
-@Client.on_message(filters.incoming & filters.command(['source_code6']) & filters.private)
+@app.on_message(filters.incoming & filters.command(['start']) & filters.private)
 async def _source_code(client, message):
     chat_id = message.from_user.id
     # Adding to DB
